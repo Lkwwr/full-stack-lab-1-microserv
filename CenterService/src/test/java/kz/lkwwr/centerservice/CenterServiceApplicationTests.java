f3cbc40e4f4a398269a6b96626edd961e482f833
lkwwr <ddreamer.july86@gmail.com>
2023-11-26 19:51:03 +0600

    init

---
+++ CenterService/src/test/java/kz/lkwwr/centerservice/CenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.lkwwr.centerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
